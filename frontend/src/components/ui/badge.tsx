f0577ccf1cc313bad5fe636f268d645369eee462
Ege Uysal <hello@egeuysal.com>
2025-09-19 19:53:16 -0500

    Debug styling

--- frontend/src/components/ui/badge.tsx
+++ frontend/src/components/ui/badge.tsx
@@ -5,13 +5,13 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const badgeVariants = cva(
-  'transition-colors duration-200 ease-in-out disabled:bg-neutral-700 disabled:dark:bg-neutral-300 disabled:dark:text-neutral-900 font-normal !text-small py-xs px-sm rounded-md',
+  'disabled:bg-neutral-700 disabled:dark:bg-neutral-300 disabled:dark:text-neutral-900 font-normal !text-small py-xs px-sm rounded-md !cursor-default',
   {
     variants: {
       variant: {
-        default: 'bg-primary-700 hover:bg-primary-900 text-neutral-100',
-        destructive: 'bg-error-700 hover:bg-error-900 text-neutral-100',
-        outline: 'border border-primary-700 hover:bg-primary-300 hover:dark:bg-primary-900',
+        default: 'bg-primary-700 text-neutral-100',
+        destructive: 'bg-error-700 text-neutral-100',
+        outline: 'border border-primary-700',
       },
     },
     defaultVariants: {
